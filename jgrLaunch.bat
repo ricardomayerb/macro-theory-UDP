41d82b069585b136c68b1be225fd450c2eaa7b4a
Ricardo Mayer <ricardomayerb@gmail.com>
2012-09-03 13:43:51 -0400

    bla

---
+++ jgrLaunch.bat
@@ -0,0 +1,4 @@
+set R_HOME=C:\PROGRA~1\R\R-215~1.1
+set R_LIBS=C:\PROGRA~1\R\R-215~1.1\library
+set R_LIBS_USER=C:\PROGRA~1\R\R-215~1.1\library;C:\PROGRA~1\RStudio\R\library
+jgr-1_62-x64.exe --rhome=C:\PROGRA~1\R\R-215~1.1 --libpath=C:\PROGRA~1\R\R-215~1.1\library
